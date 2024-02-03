b53f5210dbb74304864e4977d99ca32612a36869
Sergio Orejarena <sergioorueda@gmail.com>
2024-02-03 18:07:39 -0500

    CardList added

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+  <h5>Search:</h5>
+  <input type="text"
+    class="form-control"
+    placeholder="Search Gifs ..."
+  >
+  `,
+})
+
+export class SearchBoxComponent {
+
+}
